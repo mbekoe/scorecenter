abbcb858b71e8ac21800878377e9b521dcedaa56
frpons42 <frpons42@b9606326-8a1a-11de-8988-25e8a898b690>
2011-11-04 21:31:23 +0000

    Add Dictionaries for scores

--- trunk/ScoreCenter/ScoreCenter/Editor/GenericScoreEditor.cs
+++ trunk/ScoreCenter/ScoreCenter/Editor/GenericScoreEditor.cs
@@ -34,11 +34,18 @@ namespace MediaPortal.Plugin.ScoreCenter.Editor
 {
     public partial class GenericScoreEditor : BaseScoreEditor
     {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
         public GenericScoreEditor()
         {
             InitializeComponent();
         }
 
+        public override Type GetScoreType()
+        {
+            return typeof(GenericScore);
+        }
         public override bool HasTest
         {
             get
@@ -46,9 +53,151 @@ namespace MediaPortal.Plugin.ScoreCenter.Editor
                 return true;
             }
         }
-        public override Type GetScoreType()
+        public override void LoadScore(BaseScore baseScore, ScoreCenter center)
         {
-            return typeof(GenericScore);
+            m_center = center;
+            UpdateStyleList();
+
+            GenericScore score = baseScore as GenericScore;
+            if (score == null)
+                throw new NullReferenceException("Not a generic score!");
+
+            errorProvider1.Clear();
+
+            // always clear
+            tbxScore.Clear();
+            tbxUrl.Clear();
+            tbxXpath.Clear();
+            tbxEncoding.Clear();
+            tbxHeaders.Clear();
+            tbxSizes.Clear();
+            tbxSkip.Clear();
+            tbxMaxLines.Clear();
+            tbxElement.Clear();
+            ckxUseTheader.Checked = false;
+            ckxUseCaption.Checked = false;
+            ckxNewLine.Checked = false;
+            ckxAllowWrapping.Checked = false;
+            ckxLive.Checked = false;
+            tbxLiveFormat.Clear();
+            tbxDictionary.Clear();
+
+            grdRule.Rows.Clear();
+            grdRule.Enabled = false;
+
+            // score
+            EnableControls();
+            tbxLiveFormat.Enabled = false;
+
+            ParsingOptions options = score.GetParseOption();
+            tbxScore.Text = score.Name;
+
+            tbxUrl.Text = score.Url;
+            tbxXpath.Text = score.XPath;
+            tbxEncoding.Text = score.Encoding;
+            tbxHeaders.Text = score.Headers;
+            tbxSizes.Text = score.Sizes;
+            tbxSkip.Text = score.Skip.ToString();
+            tbxMaxLines.Text = score.MaxLines.ToString();
+            tbxElement.Text = score.Element;
+            tbxDictionary.Text = score.Dictionary;
+
+            if (score.LiveConfig != null)
+            {
+                ckxLive.Checked = score.LiveConfig.enabled;
+                tbxLiveFormat.Text = score.LiveConfig.Value;
+            }
+
+            ckxUseTheader.Checked = GenericScore.CheckParsingOption(options, ParsingOptions.UseTheader);
+            ckxUseCaption.Checked = GenericScore.CheckParsingOption(options, ParsingOptions.Caption);
+            ckxNewLine.Checked = GenericScore.CheckParsingOption(options, ParsingOptions.NewLine);
+            ckxAllowWrapping.Checked = GenericScore.CheckParsingOption(options, ParsingOptions.WordWrap);
+            ckxReverseOrder.Checked = GenericScore.CheckParsingOption(options, ParsingOptions.Reverse);
+            cbxBetweenElements.SelectedValue = score.BetweenElts;
+            tbxScoreId.Text = score.Id;
+
+            grdRule.Enabled = true;
+            SetRules(score, center);
+        }
+        public override bool SaveScore(ref BaseScore baseScore)
+        {
+            if (!CheckData())
+                return false;
+
+            GenericScore score = baseScore as GenericScore;
+
+            score.Name = tbxScore.Text;
+            score.Url = tbxUrl.Text;
+            score.XPath = tbxXpath.Text;
+            score.Headers = tbxHeaders.Text;
+            score.Sizes = tbxSizes.Text;
+            score.Encoding = tbxEncoding.Text;
+            score.Element = tbxElement.Text.Trim();
+            score.Dictionary = tbxDictionary.Text.Trim();
+
+            score.SetParseOption(ckxUseCaption.Checked, ckxUseTheader.Checked,
+                ckxNewLine.Checked, ckxAllowWrapping.Checked, ckxReverseOrder.Checked);
+            score.BetweenElts = (BetweenElements)Enum.Parse(typeof(BetweenElements), cbxBetweenElements.SelectedValue.ToString());
+
+            if (tbxSkip.Text.Length == 0) score.Skip = 0;
+            else score.Skip = int.Parse(tbxSkip.Text);
+
+            if (tbxMaxLines.Text.Length == 0) score.MaxLines = 0;
+            else score.MaxLines = int.Parse(tbxMaxLines.Text);
+
+            if (ckxLive.Checked == false && tbxLiveFormat.Text.Length == 0)
+            {
+                score.LiveConfig = null;
+            }
+            else
+            {
+                score.LiveConfig = new LiveConfig();
+                score.LiveConfig.enabled = ckxLive.Checked;
+                score.LiveConfig.Value = tbxLiveFormat.Text;
+            }
+
+            SaveRules(score);
+            return true;
+        }
+        public override void AlignColumn(Point pt, ContentAlignment alignement)
+        {
+            if (this.TestPanel == null)
+                return;
+
+            Control ctr = this.TestPanel.GetChildAtPoint(pt);
+            Label lbl = ctr as Label;
+
+            if (lbl == null || lbl.Tag == null)
+                return;
+
+            int columnIndex = Convert.ToInt32(lbl.Tag);
+            if (tbxSizes.Text.Length > 0)
+            {
+                ColumnDisplay[] dd = ColumnDisplay.GetSizes(tbxSizes.Text);
+                dd[columnIndex].Alignement = ConvertAlignment(alignement);
+
+                tbxSizes.Text = Tools.SizesToText(dd);
+            }
+
+            //lbl.TextAlign = alignement;
+
+            foreach (Label c in this.TestPanel.Controls)
+            {
+                if (c == null || c.Tag == null) continue;
+                int col = Convert.ToInt32(c.Tag);
+                if (col == columnIndex)
+                    c.TextAlign = alignement;
+            }
+        }
+        public override bool CheckData()
+        {
+            bool result = CheckTextBox(tbxScore, lblName, true);
+            result &= CheckTextBox(tbxUrl, lblUrl, true);
+            result &= CheckTextBox(tbxXpath, lblXPath, true);
+            result &= CheckNumber(tbxSkip, lblSkip, false);
+            result &= CheckNumber(tbxMaxLines, lblMaxLines, false);
+
+            return result;
         }
 
         private void SetRules(GenericScore score)
@@ -150,36 +299,6 @@ namespace MediaPortal.Plugin.ScoreCenter.Editor
 
             lblTotalSize.Text = String.Format("Total Size = {0}", res);
         }
-        public override void AlignColumn(Point pt, ContentAlignment alignement)
-        {
-            if (this.TestPanel == null)
-                return;
-
-            Control ctr = this.TestPanel.GetChildAtPoint(pt);
-            Label lbl = ctr as Label;
-
-            if (lbl == null || lbl.Tag == null)
-                return;
-
-            int columnIndex = Convert.ToInt32(lbl.Tag);
-            if (tbxSizes.Text.Length > 0)
-            {
-                ColumnDisplay[] dd = ColumnDisplay.GetSizes(tbxSizes.Text);
-                dd[columnIndex].Alignement = ConvertAlignment(alignement);
-
-                tbxSizes.Text = Tools.SizesToText(dd);
-            }
-
-            //lbl.TextAlign = alignement;
-
-            foreach (Label c in this.TestPanel.Controls)
-            {
-                if (c == null || c.Tag == null) continue;
-                int col = Convert.ToInt32(c.Tag);
-                if (col == columnIndex)
-                    c.TextAlign = alignement;
-            }
-        }
         private static ColumnDisplay.Alignment ConvertAlignment(ContentAlignment align)
         {
             if (align == ContentAlignment.MiddleRight)
@@ -206,122 +325,6 @@ namespace MediaPortal.Plugin.ScoreCenter.Editor
             {
                 grdRule[e.ColumnIndex, e.RowIndex].Value = String.Empty;
             }
-        }
-
-        public override bool CheckData()
-        {
-            bool result = CheckTextBox(tbxScore, lblName, true);
-            result &= CheckTextBox(tbxUrl, lblUrl, true);
-            result &= CheckTextBox(tbxXpath, lblXPath, true);
-            result &= CheckNumber(tbxSkip, lblSkip, false);
-            result &= CheckNumber(tbxMaxLines, lblMaxLines, false);
-
-            return result;
-        }
-
-        public override void LoadScore(BaseScore baseScore, ScoreCenter center)
-        {
-            m_center = center;
-            UpdateStyleList();
-
-            GenericScore score = baseScore as GenericScore;
-            if (score == null)
-                throw new NullReferenceException("Not a generic score!");
-
-            errorProvider1.Clear();
-
-            // always clear
-            tbxScore.Clear();
-            tbxUrl.Clear();
-            tbxXpath.Clear();
-            tbxEncoding.Clear();
-            tbxHeaders.Clear();
-            tbxSizes.Clear();
-            tbxSkip.Clear();
-            tbxMaxLines.Clear();
-            tbxElement.Clear();
-            ckxUseTheader.Checked = false;
-            ckxUseCaption.Checked = false;
-            ckxNewLine.Checked = false;
-            ckxAllowWrapping.Checked = false;
-            ckxLive.Checked = false;
-            tbxLiveFormat.Clear();
-
-            grdRule.Rows.Clear();
-            grdRule.Enabled = false;
-
-            // score
-            EnableControls();
-            tbxLiveFormat.Enabled = false;
-
-            ParsingOptions options = score.GetParseOption();
-            tbxScore.Text = score.Name;
-
-            tbxUrl.Text = score.Url;
-            tbxXpath.Text = score.XPath;
-            tbxEncoding.Text = score.Encoding;
-            tbxHeaders.Text = score.Headers;
-            tbxSizes.Text = score.Sizes;
-            tbxSkip.Text = score.Skip.ToString();
-            tbxMaxLines.Text = score.MaxLines.ToString();
-            tbxElement.Text = score.Element;
-
-            if (score.LiveConfig != null)
-            {
-                ckxLive.Checked = score.LiveConfig.enabled;
-                tbxLiveFormat.Text = score.LiveConfig.Value;
-            }
-
-            ckxUseTheader.Checked = GenericScore.CheckParsingOption(options, ParsingOptions.UseTheader);
-            ckxUseCaption.Checked = GenericScore.CheckParsingOption(options, ParsingOptions.Caption);
-            ckxNewLine.Checked = GenericScore.CheckParsingOption(options, ParsingOptions.NewLine);
-            ckxAllowWrapping.Checked = GenericScore.CheckParsingOption(options, ParsingOptions.WordWrap);
-            ckxReverseOrder.Checked = GenericScore.CheckParsingOption(options, ParsingOptions.Reverse);
-            cbxBetweenElements.SelectedValue = score.BetweenElts;
-            tbxScoreId.Text = score.Id;
-
-            grdRule.Enabled = true;
-            SetRules(score, center);
-        }
-
-        public override bool SaveScore(ref BaseScore baseScore)
-        {
-            if (!CheckData())
-                return false;
-
-            GenericScore score = baseScore as GenericScore;
-
-            score.Name = tbxScore.Text;
-            score.Url = tbxUrl.Text;
-            score.XPath = tbxXpath.Text;
-            score.Headers = tbxHeaders.Text;
-            score.Sizes = tbxSizes.Text;
-            score.Encoding = tbxEncoding.Text;
-            score.Element = tbxElement.Text;
-
-            score.SetParseOption(ckxUseCaption.Checked,  ckxUseTheader.Checked,
-                ckxNewLine.Checked, ckxAllowWrapping.Checked, ckxReverseOrder.Checked);
-            score.BetweenElts = (BetweenElements)Enum.Parse(typeof(BetweenElements), cbxBetweenElements.SelectedValue.ToString());
-
-            if (tbxSkip.Text.Length == 0) score.Skip = 0;
-            else score.Skip = int.Parse(tbxSkip.Text);
-
-            if (tbxMaxLines.Text.Length == 0) score.MaxLines = 0;
-            else score.MaxLines = int.Parse(tbxMaxLines.Text);
-
-            if (ckxLive.Checked == false && tbxLiveFormat.Text.Length == 0)
-            {
-                score.LiveConfig = null;
-            }
-            else
-            {
-                score.LiveConfig = new LiveConfig();
-                score.LiveConfig.enabled = ckxLive.Checked;
-                score.LiveConfig.Value = tbxLiveFormat.Text;
-            }
-
-            SaveRules(score);
-            return true;
         }
         
         private void EnableControls(params Control[] controls)
