fd8d99ed874515c4abbd38335c9cf8443c8ac158
frpons42@gmail.com <frpons42@gmail.com@b9606326-8a1a-11de-8988-25e8a898b690>
2014-05-04 08:50:42 +0000

    more re-use

--- trunk/ScoreCenter/ScoreCenter/Tools.cs
+++ trunk/ScoreCenter/ScoreCenter/Tools.cs
@@ -25,6 +25,9 @@
 
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -525,9 +528,10 @@ namespace MediaPortal.Plugin.ScoreCenter
             result = result.Replace("{YY-YY+1}", String.Format("{0}-{1}", now.ToString("yy"), next.ToString("yy")));
             result = result.Replace("{YYYY-YY+1}", String.Format("{0}-{1}", now.ToString("yyyy"), next.ToString("yy")));
             result = result.Replace("{YYYY-YYYY+1}", String.Format("{0}-{1}", now.ToString("yyyy"), next.ToString("yyyy")));
+            result = result.Replace("{YY}", String.Format("{0}", now.ToString("yy")));
             result = result.Replace("{YY+1}", String.Format("{0}", next.ToString("yy")));
-            result = result.Replace("{YYYY+1}", String.Format("{0}", next.ToString("yyyy")));
             result = result.Replace("{YY-1}", String.Format("{0}", now.ToString("yy")));
+            result = result.Replace("{YYYY+1}", String.Format("{0}", next.ToString("yyyy")));
             result = result.Replace("{YYYY-1}", String.Format("{0}", now.ToString("yyyy")));
 
             // subst parameters
@@ -559,6 +563,52 @@ namespace MediaPortal.Plugin.ScoreCenter
 
             return result;
         }
+
+        public static Image FixedBitmapSize(Bitmap imgPhoto, int width, int height, Color backColor)
+        {
+            int sourceWidth = imgPhoto.Width;
+            int sourceHeight = imgPhoto.Height;
+            int sourceX = 0;
+            int sourceY = 0;
+            int destX = 0;
+            int destY = 0;
+
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+
+            nPercentW = ((float)width / (float)sourceWidth);
+            nPercentH = ((float)height / (float)sourceHeight);
+            if (nPercentH < nPercentW)
+            {
+                nPercent = nPercentH;
+                destX = System.Convert.ToInt16((width - (sourceWidth * nPercent)) / 2);
+            }
+            else
+            {
+                nPercent = nPercentW;
+                destY = System.Convert.ToInt16((height - (sourceHeight * nPercent)) / 2);
+            }
+
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap bmPhoto = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+            bmPhoto.SetResolution(imgPhoto.HorizontalResolution, imgPhoto.VerticalResolution);
+
+            Graphics grPhoto = Graphics.FromImage(bmPhoto);
+            grPhoto.Clear(backColor);
+            grPhoto.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+            grPhoto.DrawImage(imgPhoto,
+                new Rectangle(destX, destY, destWidth, destHeight),
+                new Rectangle(sourceX, sourceY, sourceWidth, sourceHeight),
+                GraphicsUnit.Pixel);
+
+            grPhoto.Dispose();
+            return bmPhoto;
+        }
+
     }
 
     /// <summary>
