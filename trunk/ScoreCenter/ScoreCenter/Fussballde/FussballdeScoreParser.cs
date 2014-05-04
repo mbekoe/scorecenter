1654dc9b0b227a85d44ee53776c919ac36ee8246
frpons42@gmail.com <frpons42@gmail.com@b9606326-8a1a-11de-8988-25e8a898b690>
2014-05-04 13:08:07 +0000

    Tweak Fussballde

--- trunk/ScoreCenter/ScoreCenter/Fussballde/FussballdeScoreParser.cs
+++ trunk/ScoreCenter/ScoreCenter/Fussballde/FussballdeScoreParser.cs
@@ -41,28 +41,28 @@ namespace MediaPortal.Plugin.ScoreCenter.Parser
         private const string MAIN_URL = @"http://ergebnisdienst.fussball.de";
         private const string EMBLEM_PATH = "//a[@class='egmAssociationLogo']/img";
 
-        private const string IMG_REFEREE = @"Misc\Referee.png";
-        private const string IMG_TOPSCORER = @"Football\Top Scorers.png";
-        private const string IMG_TABELLE = @"Misc\score.png";
-        private const string IMG_TABELLE_HOME = @"Misc\tabelle_home.png";
-        private const string IMG_TABELLE_AWAY = @"Misc\tabelle_away.png";
-        private const string IMG_TABELLE_ROUND1 = @"Misc\tabelle_round1.png";
-        private const string IMG_TABELLE_ROUND2 = @"Misc\tabelle_round2.png";
-        private const string IMG_RESULTS = @"Results.png";
+        private const string IMG_REFEREE = @"Misc\Referee";
+        private const string IMG_TOPSCORER = @"Football\Top Scorers";
+        private const string IMG_TABELLE = @"Misc\score";
+        private const string IMG_TABELLE_HOME = @"Misc\table_home";
+        private const string IMG_TABELLE_AWAY = @"Misc\table_away";
+        private const string IMG_TABELLE_ROUND1 = @"Misc\table_round1";
+        private const string IMG_TABELLE_ROUND2 = @"Misc\table_round2";
+        private const string IMG_RESULTS = @"Results";
 
         private const string XPATH_RESULTS = "//table[@class='egmSnippetContent egmMatchesTable']";
         private const string XPATH_TABELLE = "//table[@class='egmSnippetContent']";
-        private const string XPATH_FAIRNESS = "//table[@class='egmSnippetContent']";
+        private const string XPATH_FAIRPLAY = "//table[@class='egmSnippetContent']";
         private const string XPATH_SCORER = "//table[@class='egmSnippetContent']";
 
         private const string SIZES_RESULTS = "-15,-12,20,+1,-20,-8";
         private const string SIZES_TABELLE = "3,-25,3,3,3,3,6,4,4,0";
-        private const string SIZES_FAIRNESS = "3,-25,3,3,3,3,3,3,3,5";
+        private const string SIZES_FAIRPLAY = "3,-25,3,3,3,3,3,3,3,5";
         private const string SIZES_SCORER = "4,-25,-25,4";
 
         private const string HEADER_RESULTS = "Date,Time";
         private const string HEADER_TABELLE = ",Team,Pl,W,D,L,Goals,+/-,Pts";
-        private const string HEADER_FAIRNESS = ",Team,Pl,Y,YR,R,T,U,Pts,R";
+        private const string HEADER_FAIRPLAY = ",Team,Pl,Y,YR,R,T,U,Pts,R";
         private const string HEADER_SCORER = ",Player,Team,Goals";
 
         private const string CUT_BEFORE = "Weiter zur";
@@ -107,6 +107,7 @@ namespace MediaPortal.Plugin.ScoreCenter.Parser
             sc = GenericScore.CreateNewScore(score.Id, C_KEY_RESULTS, "Results", XPATH_RESULTS, IMG_RESULTS, "", index++);
             sc.Url = GetUrl(score.Url, C_KEY_RESULTS);
             sc.Skip = 1;
+            sc.Dictionary = "Fussballde";
             sc.Sizes = ScoreCenter.GetParameter(parameters, "Fde.SizesResults", SIZES_RESULTS);
             sc.Headers = ScoreCenter.GetParameter(parameters, "Fde.HeaderResults", HEADER_RESULTS);
             sc.AddRule(3, Operation.Contains, CUT_BEFORE, RuleAction.CutBefore, "");
@@ -115,9 +116,10 @@ namespace MediaPortal.Plugin.ScoreCenter.Parser
             sc.AddRule(1, Operation.Contains, "Verlegte Spiele", RuleAction.MergeCells, "");
             scores.Add(sc);
 
-            sc = GenericScore.CreateNewScore(score.Id, "tabelle", "tabelle", XPATH_TABELLE, IMG_TABELLE, "", index++);
+            sc = GenericScore.CreateNewScore(score.Id, C_KEY_TABELLE, "Standings", XPATH_TABELLE, IMG_TABELLE, "", index++);
             sc.Url = GetUrl(score.Url, C_KEY_TABELLE);
             sc.Skip = 1;
+            sc.Dictionary = "Fussballde";
             sc.Sizes = ScoreCenter.GetParameter(parameters, "Fde.SizesTabelle", SIZES_TABELLE);
             sc.Headers = ScoreCenter.GetParameter(parameters, "Fde.HeaderTabelle", HEADER_TABELLE);
             sc.AddRule(2, Operation.Contains, CUT_BEFORE, RuleAction.CutBefore, "");
@@ -127,18 +129,20 @@ namespace MediaPortal.Plugin.ScoreCenter.Parser
 
             if (details.Contains("runde"))
             {
-                sc = GenericScore.CreateNewScore(score.Id, "hinrunde", "Hin Runde", XPATH_TABELLE, IMG_TABELLE_HOME, "0", index++);
+                sc = GenericScore.CreateNewScore(score.Id, "hinrunde", "1st Round", XPATH_TABELLE, IMG_TABELLE_ROUND1, "0", index++);
                 sc.Url = GetUrl(score.Url, C_KEY_RUNDE);
                 sc.Skip = 1;
+                sc.Dictionary = "Fussballde";
                 sc.Sizes = ScoreCenter.GetParameter(parameters, "Fde.SizesTabelle", SIZES_TABELLE);
                 sc.Headers = ScoreCenter.GetParameter(parameters, "Fde.HeaderTabelle", HEADER_TABELLE);
                 sc.AddRule(2, Operation.Contains, CUT_BEFORE, RuleAction.CutBefore, "");
                 sc.AddHighlightRule(score.Highlights, 2, RuleAction.FormatLine);
                 scores.Add(sc);
 
-                sc = GenericScore.CreateNewScore(score.Id, "rueckrunde", "Rueck Runde", XPATH_TABELLE, IMG_TABELLE_AWAY, "1", index++);
+                sc = GenericScore.CreateNewScore(score.Id, "rueckrunde", "2nd Round", XPATH_TABELLE, IMG_TABELLE_ROUND2, "1", index++);
                 sc.Url = GetUrl(score.Url, C_KEY_RUNDE);
                 sc.Skip = 1;
+                sc.Dictionary = "Fussballde";
                 sc.Sizes = ScoreCenter.GetParameter(parameters, "Fde.SizesTabelle", SIZES_TABELLE);
                 sc.Headers = ScoreCenter.GetParameter(parameters, "Fde.HeaderTabelle", HEADER_TABELLE);
                 sc.AddRule(2, Operation.Contains, CUT_BEFORE, RuleAction.CutBefore, "");
@@ -146,18 +150,20 @@ namespace MediaPortal.Plugin.ScoreCenter.Parser
                 scores.Add(sc);
             }
 
-            sc = GenericScore.CreateNewScore(score.Id, "heim", "Heim", XPATH_TABELLE, IMG_TABELLE_ROUND1, "0", index++);
+            sc = GenericScore.CreateNewScore(score.Id, "heim", "Home", XPATH_TABELLE, IMG_TABELLE_HOME, "0", index++);
             sc.Url = GetUrl(score.Url, C_KEY_HEIMAUS);
             sc.Skip = 1;
+            sc.Dictionary = "Fussballde";
             sc.Sizes = ScoreCenter.GetParameter(parameters, "Fde.SizesTabelle", SIZES_TABELLE);
             sc.Headers = ScoreCenter.GetParameter(parameters, "Fde.HeaderTabelle", HEADER_TABELLE);
             sc.AddRule(2, Operation.Contains, CUT_BEFORE, RuleAction.CutBefore, "");
             sc.AddHighlightRule(score.Highlights, 2, RuleAction.FormatLine);
             scores.Add(sc);
 
-            sc = GenericScore.CreateNewScore(score.Id, "auswaerts", "Auswaerts", XPATH_TABELLE, IMG_TABELLE_ROUND2, "1", index++);
+            sc = GenericScore.CreateNewScore(score.Id, "auswaerts", "Away", XPATH_TABELLE, IMG_TABELLE_AWAY, "1", index++);
             sc.Url = GetUrl(score.Url, C_KEY_HEIMAUS);
             sc.Skip = 1;
+            sc.Dictionary = "Fussballde";
             sc.Sizes = ScoreCenter.GetParameter(parameters, "Fde.SizesTabelle", SIZES_TABELLE);
             sc.Headers = ScoreCenter.GetParameter(parameters, "Fde.HeaderTabelle", HEADER_TABELLE);
             sc.AddRule(2, Operation.Contains, CUT_BEFORE, RuleAction.CutBefore, "");
@@ -166,11 +172,12 @@ namespace MediaPortal.Plugin.ScoreCenter.Parser
 
             if (details.Contains("fairness"))
             {
-                sc = GenericScore.CreateNewScore(score.Id, "fairnesstabelle", "Fairness", XPATH_FAIRNESS, IMG_REFEREE, "", index++);
+                sc = GenericScore.CreateNewScore(score.Id, C_KEY_FAIRNESS, "Fair Play", XPATH_FAIRPLAY, IMG_REFEREE, "", index++);
                 sc.Url = GetUrl(score.Url, C_KEY_FAIRNESS);
                 sc.Skip = 1;
-                sc.Sizes = ScoreCenter.GetParameter(parameters, "Fde.SizesFairness", SIZES_FAIRNESS);
-                sc.Headers = ScoreCenter.GetParameter(parameters, "Fde.HeaderFairness", HEADER_FAIRNESS);
+                sc.Dictionary = "Fussballde";
+                sc.Sizes = ScoreCenter.GetParameter(parameters, "Fde.SizesFairplay", SIZES_FAIRPLAY);
+                sc.Headers = ScoreCenter.GetParameter(parameters, "Fde.HeaderFairplay", HEADER_FAIRPLAY);
                 sc.AddRule(2, Operation.Contains, CUT_BEFORE, RuleAction.CutBefore, "");
                 sc.AddHighlightRule(score.Highlights, 2, RuleAction.FormatLine);
                 scores.Add(sc);
@@ -178,9 +185,10 @@ namespace MediaPortal.Plugin.ScoreCenter.Parser
 
             if (details.Contains("torjaeger"))
             {
-                sc = GenericScore.CreateNewScore(score.Id, "torjaeger", "Top Scorer", XPATH_SCORER, IMG_TOPSCORER, "", index++);
+                sc = GenericScore.CreateNewScore(score.Id, C_KEY_SCORER, "Top Scorers", XPATH_SCORER, IMG_TOPSCORER, "", index++);
                 sc.Url = GetUrl(score.Url, C_KEY_SCORER);
                 sc.Skip = 1;
+                sc.Dictionary = "Fussballde";
                 sc.Sizes = ScoreCenter.GetParameter(parameters, "Fde.SizesScorer", SIZES_SCORER);
                 sc.Headers = ScoreCenter.GetParameter(parameters, "Fde.HeaderScorer", HEADER_SCORER);
                 sc.AddRule(3, Operation.Contains, CUT_BEFORE, RuleAction.CutBefore, "");
